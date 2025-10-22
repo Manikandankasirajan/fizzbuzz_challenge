e55a4b1f4e5cc7b1b5530794d41a816a6cf06b97
Manikandankasirajan <itsmanikandankasirajan@gmail.com>
2025-10-22 17:39:16 +0530

    feat : added a sample test case and a fuction to pass the test

---
+++ src/fizzbuzz.js
@@ -0,0 +1,5 @@
+function fizzbuzz(num) {
+	return num % 3 === 0 ? "FIZZ" : null;
+}
+
+export default fizzbuzz;
---
+++ src/tests/fizzbuzz.test.js
@@ -0,0 +1,5 @@
+import fizzbuzz from "../fizzbuzz.js";
+
+test('returns "Fizz" for multiples of 3', () => {
+	expect(fizzbuzz(3)).toBe("FIZZ");
+});
